a85e088d8bbe68102801732a585c976ba9b44f6b
Caleb Stevenson <cgrahamstevenson@gmail.com>
2016-11-14 23:06:24 -0800

    add robolectric unit testing

---
+++ app/src/test/java/com/epicodus/restaurants/MainActivityTest.java
@@ -0,0 +1,46 @@
+package com.epicodus.restaurants;
+
+import android.content.Intent;
+import android.os.Build;
+import android.widget.TextView;
+
+import org.apache.tools.ant.Main;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static junit.framework.Assert.assertTrue;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+/**
+ * Created by CGrahamS on 11/14/16.
+ */
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appHeader = (TextView) activity.findViewById(R.id.appHeader);
+        assertTrue("¡Restaurants!".equals(appHeader.getText().toString()));
+    }
+
+    @Test
+    public void secondActivityStarted() {
+        activity.findViewById(R.id.zipSubmit).performClick();
+        Intent expectedIntent = new Intent(activity, RestaurantActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+}
